b8773cad99abe14d9d4bd17997d4ad0f641f9209
Jack Martin <jackmartin.dev@gmail.com>
2023-12-13 17:41:13 +0900

    chore: Cleaned up template

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,9 @@
 function App(): JSX.Element {
-  return (
-    <div className="container">
-      <h3>Hey you</h3>
-    </div>
-  )
+    return (
+        <div className="container">
+            <h3>Hey you</h3>
+        </div>
+    )
 }
 
 export default App
