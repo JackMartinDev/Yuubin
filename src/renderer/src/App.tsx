3279ad48dbe4b15085d7af517621bab3032c3310
Jack Martin <jackmartin.dev@gmail.com>
2023-12-13 22:59:56 +0900

    feat: Added codemirror and confiured with lang + theme

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,18 @@
+import CodeMirror from "@uiw/react-codemirror"
+import { json } from "@codemirror/lang-json"
+import { tokyoNightStorm } from "@uiw/codemirror-theme-tokyo-night-storm" 
+import { useCallback, useState } from "react"
+
 function App(): JSX.Element {
+    const [value, setValue] = useState("console.log('Hello World!');")
+    const onChange = useCallback((val, viewUpdate) => {
+        console.log("val: ", val);
+        setValue(val);
+    }, []);
     return (
         <div className="container">
             <h3>Hey you</h3>
+            <CodeMirror value={value} height="200px" theme={tokyoNightStorm} extensions={[json()]} onChange={onChange} />
         </div>
     )
 }
