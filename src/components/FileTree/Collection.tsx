c738de1c86aea5ad26f3fcfb234b9757aa2c0581
Jack Martin <jackmartin.dev@gmail.com>
2024-02-03 17:44:41 +0900

    feat: Added basic styling and accordion functionality

--- src/components/FileTree/Collection.tsx
+++ src/components/FileTree/Collection.tsx
@@ -1,15 +1,25 @@
-
-
+import { useState } from "react";
+import classes from "./Collection.module.css" 
+import cx from "clsx"
 type Props = {
     collection: Collection 
 }
 
 const Collection = ({collection}: Props): JSX.Element => {
+    const [isToggled, setIsToggled] = useState(false);
+    
+    const toggle = () => {
+        setIsToggled(!isToggled)
+    }
+
     return(
-    <>
-        <p>{collection.name}</p>
-        {collection.requests.map((request) => (<ul>{request.url}</ul>))}
-    </>)
+        <div className={classes.accordion} onClick={toggle}>
+            <p>{collection.name}</p>
+            <div className={cx(classes.content, {[classes.show]: isToggled})}>
+                {collection.requests.map((request) => (<li>{request.url}</li>))}
+            </div>
+        </div>
+    )
 }
 
 export default Collection
