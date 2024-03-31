1192b7b3ccc4f323f2a24265c06e0acfd50d4981
Jack Martin <jackmartin.dev@gmail.com>
2024-03-31 12:57:57 +0900

    feat: Add tabs to client

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Client from "./components/Client/Client"
 import FileTree from "./components/FileTree/FileTree";
 import classes from "./App.module.css"
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import { CloseButton, Flex, Tabs, Text } from "@mantine/core";
 function App(): JSX.Element {
     return (
         <div  className={classes.container}>
@@ -14,7 +15,33 @@ function App(): JSX.Element {
                 </Panel>
                 <PanelResizeHandle />
                 <Panel defaultSize={90} minSize={70}>
-                    <Client/>
+                    <Tabs variant="outline" defaultValue="tab 1">
+                        <Tabs.List>
+                            <Tabs.Tab value="tab 1" p="xs">
+                                <Flex align="center" gap="xs">
+                                    <Text>Tab 1</Text> 
+                                    <CloseButton size="sm"/>
+                                </Flex>
+                            </Tabs.Tab>
+                            <Tabs.Tab value="tab 2" p="xs">
+                                <Flex align="center" gap="xs">
+                                    <Text>Tab 2</Text> 
+                                    <CloseButton size="sm"/>
+                                </Flex>
+
+                            </Tabs.Tab>
+                        </Tabs.List>
+
+                        <Tabs.Panel value="tab 1" mt="sm">
+                            <Client/>
+                        </Tabs.Panel>
+
+                        <Tabs.Panel value="tab 2" mt="sm">
+                            <Client/>
+                        </Tabs.Panel>
+
+                    </Tabs>
+
                 </Panel>
             </PanelGroup>
         </div>
