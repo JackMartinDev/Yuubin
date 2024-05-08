e18980d9bee6cc31a93b4694d873a53ef95fbae1
Jack Martin <jackmartin.dev@gmail.com>
2024-05-08 14:00:02 +0900

    feat: Add component to display the coloured method verbs

---
+++ src/components/MethodIcon.tsx
@@ -0,0 +1,27 @@
+import { Text } from "@mantine/core";
+
+interface Props {
+    method: HttpVerb
+}
+
+const MethodIcon = ({method}: Props) => {
+    let color;
+    switch (method) {
+        case "GET":
+            color = "#1a752e"
+            break;
+        case "POST":
+            color = "#6134eb"
+            break;
+        case "DELETE":
+            color = "#b51e0d"
+            break;
+        default:
+            color = "#ed9015"
+            break;
+    }
+
+    return <Text size="xs" c={color} fw={500} display="inline">{method}</Text>
+}
+
+export default MethodIcon
