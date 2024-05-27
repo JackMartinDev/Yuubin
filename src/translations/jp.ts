f15d1829417a7de9323075a9371e680fa564a493
Jack Martin <jackmartin.dev@gmail.com>
2024-05-27 15:50:42 +0900

    feat: Add Japanese translations

--- src/translations/jp.ts
+++ src/translations/jp.ts
@@ -1,3 +1,69 @@
 export const translationsJp = {
-    yuubin: "ユウビン"
+    //General
+    yuubin: "ユウビン",
+    cancel: "キャンセル",
+    create: "作成",
+    delete: "削除",
+
+    //File Tree
+    create_collection: "コレクション作成",
+    new_collection: "新しいコレクション",
+    collection_name: "コレクション名",
+    delete_collection: "コレクション削除",
+    delete_collection_warning: "コレクション「{{name}}」と配下にあるリクエストを削除します。よろしいですか？",
+    add_request: "リクエスト作成",
+    new_request: "新しいリクエスト",
+    request_name: "リクエスト名",
+    url: "URL",
+    method: "メソド",
+    run: "実行",
+    rename: "名前変更",
+    delete_request: "リクエスト削除",
+    delete_request_warning: "「{{name}}」コレクションからリクエストを削除します。よろしいですか？",
+    
+    //Settings
+    settings: "設定",
+    save_on_quit: "Save on quit",
+    preserve_open_tabs: "開いたタブを保持",
+    collection_path: "コレクションのデータパス",
+    browse: "参照",
+    display_settings: "画面設定",
+    select_language: "言語を選択してください",
+    language: "言語",
+    theme: "テーマ",
+    apply: "設定変更",
+
+    //Client
+    send: "送信",
+    query: "クエリー",
+    body: "ボディ",
+    headers: "ヘッダー",
+    auth: "認証",
+    key: "キー",
+    value: "値",
+    add: "追加",
+    token: "認証トークン",
+    pre_request_message: "上にリクエストを入力して送信してください。",
+
+    //Notifications
+    success: "成功",
+    in_development: "開発中",
+    in_development_message: "この機能は開発中です。",
+    error: "エラー",
+    unexpected_error: "予期せぬエラー",
+
+    //Success
+    create_collection_success: "コレクション作成に成功しました。",
+    delete_collection_success: "コレクション削除に成功しました。",
+    create_request_success: "リクエストン作成に成功しました。",
+    delete_request_success: "リクエスト削除に成功しました。",
+    update_config_success: "設定変更に成功しました。",
+    
+    //Error;
+
+
+    //Validation
+    collection_name_required: "コレクション名は必須項目です。",
+    request_name_required: "リクエスト名は必須項目です。",
+    data_path_required: "データパスは必須項目です。",
 }
