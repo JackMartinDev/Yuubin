30e0eba953e9e76145902312661902b70a763914
Jack Martin <jackmartin.dev@gmail.com>
2024-05-29 11:59:18 +0900

    feat: Remove splashsceen

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -459,21 +459,6 @@ fn rename_directory(collection: String, new_collection: String) -> Response{
 
 fn main() {
     tauri::Builder::default()
-        .setup(|app| {
-            let splashscreen_window = app.get_window("splashscreen").unwrap();
-            let main_window = app.get_window("main").unwrap();
-            // we perform the initialization code on a new task so the app doesn't freeze
-            tauri::async_runtime::spawn(async move {
-                // initialize your app here instead of sleeping :)
-                println!("Initializing...");
-                std::thread::sleep(std::time::Duration::from_secs(2));
-                println!("Done initializing.");
-                // After it's done, close the splashscreen and display the main window
-                splashscreen_window.close().unwrap();
-                main_window.show().unwrap();
-            });
-            Ok(())
-        })
         .invoke_handler(tauri::generate_handler![sync_files, sync_config, edit_config, delete_file, create_file, edit_file, rename_file, delete_directory, create_directory, rename_directory])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
